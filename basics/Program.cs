3df7623e94fd790521c2c3d675554e145e2b2dfc
Armen Babayan <armenbabayan1984@gmail.com>
2024-02-25 20:34:39 +0400

    changes

--- basics/Program.cs
+++ basics/Program.cs
@@ -5,6 +5,24 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+
+        //for(int i = 0; i< args.Length; i++)
+        //{
+        //    Console.WriteLine("Arg: {0}", args[i]);
+        //}
+        //foreach(string arg in args)
+        //{
+        //    Console.WriteLine(arg);
+        //}
+        string[] theArgs = Environment.GetCommandLineArgs();
+        foreach(string arg in theArgs)
+        {
+
+        }
+
+        //var value = System.Console.ReadLine();
+        //System.Console.WriteLine("This is my first code in C% " + value);
+
         // NumberTypesMembers ntm = new NumberTypesMembers();
         // ntm.GetNumberTypesMembers();\
 
@@ -14,6 +32,23 @@ internal class Program
         // ParsFromStrings parsFromStrings = new ParsFromStrings();
         // parsFromStrings.UsingTryParse();
 
-        DateTimeAndTimeSpan dtObject = new DateTimeAndTimeSpan();
+        //DateTimeAndTimeSpan dtObject = new DateTimeAndTimeSpan();
+
+        //Person p = new Person();
+        //p.Name = "John";
+        //p.Age = 40;
+        //p.Print();
+
     }
+
+    //struct Person
+    //{
+    //    public string Name { get; set; }
+    //    public int Age { get; set; }
+
+    //    public void Print()
+    //    {
+    //        Console.WriteLine($"Hello my name is {Name}, I am {Age} years old");
+    //    }
+    //}
 }
